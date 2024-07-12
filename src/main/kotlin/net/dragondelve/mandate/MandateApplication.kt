48d3c21efbc5c4667e6ef260b3345fd794640e5d
Prokhor Kalinin <inferiordev@gmail.com>
2024-07-12 16:18:54 +0000

    Initial Project Setup Commit

---
+++ src/main/kotlin/net/dragondelve/mandate/MandateApplication.kt
@@ -0,0 +1,37 @@
+/* Copyright 2024 Prokhor Kalinin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package net.dragondelve.mandate
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+import net.dragondelve.mandate.controllers.MandateController
+
+class MandateApplication : Application() {
+    override fun start(stage: Stage) {
+        val fxmlLoader = FXMLLoader(MandateApplication::class.java.getResource("mandate-main.fxml"))
+        fxmlLoader.setController(MandateController())
+        val scene = Scene(fxmlLoader.load(), 800.0, 500.0)
+        stage.title = "Mandate v0.1.0"
+        stage.scene = scene
+        stage.show()
+    }
+}
+
+fun main() {
+    Application.launch(MandateApplication::class.java)
+}
---
+++ src/main/kotlin/net/dragondelve/mandate/controllers/MandateController.kt
@@ -0,0 +1,77 @@
+/* Copyright 2024 Prokhor Kalinin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package net.dragondelve.mandate.controllers
+
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.ListView
+import javafx.scene.control.MenuItem
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.scene.layout.BorderPane
+
+class MandateController {
+
+    @FXML
+    private lateinit var createTypeColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var deleteRoleButton: Button
+
+    @FXML
+    private lateinit var deleteTypeColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var editRoleButton: Button
+
+    @FXML
+    private lateinit var editTypesMenuItem: MenuItem
+
+    @FXML
+    private lateinit var loadLocalMenuItem: MenuItem
+
+    @FXML
+    private lateinit var permissionTypeNameColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var readTypeColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var roleIdColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var roleNameColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var roleTableView: TableView<Any>
+
+    @FXML
+    private lateinit var rootPane: BorderPane
+
+    @FXML
+    private lateinit var saveLocalMenuItem: MenuItem
+
+    @FXML
+    private lateinit var updateTypeColumn: TableColumn<Any, Any>
+
+    @FXML
+    private lateinit var usersListView: ListView<Any>
+
+    @FXML
+    private fun initialize() {
+        System.out.println("Initialization Complete");
+    }
+}
